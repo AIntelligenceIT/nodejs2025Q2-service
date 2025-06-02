18c5f5168b04354bd77d6f4657e0e7edf7999682
AlanKowalzky <alan.kowalzky@gmail.com>
2025-06-02 11:13:56 +0200

    fix: after ts before fRef

--- src/artists/artists.module.ts
+++ src/artists/artists.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ArtistsService } from './artists.service';
 import { ArtistsController } from './artists.controller';
-import { AlbumsModule } from '../albums/albums.module';
 import { TracksModule } from '../tracks/tracks.module';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
-  imports: [AlbumsModule, TracksModule],
+  imports: [TracksModule, PrismaModule],
   controllers: [ArtistsController],
   providers: [ArtistsService],
   exports: [ArtistsService],
