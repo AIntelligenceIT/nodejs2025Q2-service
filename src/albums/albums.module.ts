629aeb273c78c59e9bd6c048d4ec1acfc9b09191
AlanKowalzky <alan.kowalzky@gmail.com>
2025-06-02 11:23:16 +0200

    feat: swagger doc after ts after fRef

--- src/albums/albums.module.ts
+++ src/albums/albums.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { AlbumsService } from './albums.service';
 import { AlbumsController } from './albums.controller';
 import { PrismaModule } from '../prisma/prisma.module';
+import { ArtistsModule } from '../artists/artists.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, forwardRef(() => ArtistsModule)],
   controllers: [AlbumsController],
   providers: [AlbumsService],
   exports: [AlbumsService],
--- src/artists/artists.module.ts
+++ src/artists/artists.module.ts
@@ -1,11 +1,12 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { ArtistsService } from './artists.service';
 import { ArtistsController } from './artists.controller';
+import { AlbumsModule } from '../albums/albums.module';
 import { TracksModule } from '../tracks/tracks.module';
 import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
-  imports: [TracksModule, PrismaModule],
+  imports: [forwardRef(() => AlbumsModule), TracksModule, PrismaModule],
   controllers: [ArtistsController],
   providers: [ArtistsService],
   exports: [ArtistsService],
